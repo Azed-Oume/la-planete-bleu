b735d9fe65a6c601637235b3f09056c695f8a005
Azed-Oume <oumessaoud@hotmail.fr>
2023-06-03 21:14:52 +0200

    ça avance

--- src/js/loupe.js
+++ src/js/loupe.js
@@ -1,24 +1,18 @@
-// $(document).ready(function() {
-//     $('.element-survole').hover(function() {
-//       // Créez un nouvel élément de loupe
-//       var loupe = document.createElement('div');
-//       loupe.className = 'loupe';
-//       loupe.style.position = 'absolute';
-//       loupe.style.top = $(this).offset().top + 'px';
-//       loupe.style.left = $(this).offset().left + $(this).outerWidth() + 10 + 'px';
-      
-//       // Ajoutez le nouvel élément de loupe au DOM
-//       document.body.appendChild(loupe);
-      
-//       // Déplacez la loupe en fonction du mouvement de la souris
-//       $(this).mousemove(function(event) {
-//         loupe.style.backgroundImage = 'url(' + $(this).attr('src') + ')';
-//         loupe.style.backgroundSize = ($(this).width() * 2) + 'px ' + ($(this).height() * 2) + 'px';
-//         loupe.style.backgroundPosition = (-event.pageX + $(this).offset().left) * 2 + 'px ' + (-event.pageY + $(this).offset().top) * 2 + 'px';
-//       });
-//     }, function() {
-//       // Supprimez la loupe lorsque l'événement de survol se termine
-//       $('.loupe').remove();
-//     });
-//   });
-  
+const btnMyCountElement = document.querySelector(".connection__a");
+const btnCountElement = document.querySelector(".connection__b");
+
+const sectionElementCount = document.querySelector(".section__count");
+const sectionElementCreatcount = document.querySelector(".section__creatcount");
+
+
+function btnCount() {
+    btnMyCountElement.addEventListener("click", function () {
+        sectionElementCount.style.display = "block";
+        btnCountElement.style.display = "none";
+    });
+    btnCountElement.addEventListener("click", function () {
+        sectionElementCreatcount.style.display = "block";
+        btnMyCountElement.style.display = "none";
+    });
+}
+btnCount();
---
+++ src/js/script.js
@@ -0,0 +1,56 @@
+function showdropdown() {
+    var dropdown = document.getElementById("dropdown");
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
+
+document.getElementById("dropdown").addEventListener("change", function () {
+    window.location.href = this.value;
+});
+
+const carousel = document.querySelector('.carousel');
+const slides = carousel.querySelector('.slides');
+const prevButton = carousel.querySelector('.prev-button');
+const nextButton = carousel.querySelector('.next-button');
+const stopButton = carousel.querySelector('.stop-button');
+const slideWidth = carousel.querySelector('.slide').clientWidth;
+let currentIndex = 0;
+let intervalId;
+
+function goToSlide(index) {
+    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+    currentIndex = index;
+}
+
+function goToNextSlide() {
+    if (currentIndex === slides.children.length - 1) {
+        goToSlide(0);
+    } else {
+        goToSlide(currentIndex + 1);
+    }
+}
+
+function goToPrevSlide() {
+    if (currentIndex === 0) {
+        goToSlide(slides.children.length - 1);
+    } else {
+        goToSlide(currentIndex - 1);
+    }
+}
+
+function stopCarousel() {
+    clearInterval(intervalId);
+}
+
+function startCarousel() {
+    intervalId = setInterval(goToNextSlide, 3000);
+}
+
+startCarousel();
+
+prevButton.addEventListener('click', goToPrevSlide);
+nextButton.addEventListener('click', goToNextSlide);
+stopButton.addEventListener('click', stopCarousel);

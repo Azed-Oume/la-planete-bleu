96617012a58b30c767beffa821b0a10270ce8ce5
Azed-Oume <oumessaoud@hotmail.fr>
2023-07-16 20:15:52 +0200

    quelque correction et ajout de contennue

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,24 @@
+const btnMyCountElement = document.querySelector(".connection__a");
+const btnCountElement = document.querySelector(".connection__b");
+
+const sectionElementCount = document.querySelector(".section__count");
+const sectionElementCreatcount = document.querySelector(".section__creatcount");
+
+
+function btnCount() {
+    btnMyCountElement.addEventListener("click", function () {
+        sectionElementCount.style.display = "block";
+        btnCountElement.style.display = "none";
+    });
+    btnCountElement.addEventListener("click", function () {
+        sectionElementCreatcount.style.display = "block";
+        btnMyCountElement.style.display = "none";
+    });
+}
+btnCount();
+
+
+
 function showdropdown() {
     var dropdown = document.getElementById("dropdown");
     if (dropdown.style.display === "none") {
